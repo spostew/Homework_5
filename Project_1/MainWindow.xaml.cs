f126778c975e0f5b54075b99667ce3af3e8808ba
spostew <spencerunitt0@gmail.com>
2021-09-24 14:22:12 -0700

    Add project files.

---
+++ Project_1/MainWindow.xaml.cs
@@ -0,0 +1,95 @@
+﻿/*
+ * Project_1: This program will take in the weight and height of an individual and return a color indicating how healthy their 
+ *            BMI is
+ * Name: Spencer Unitt
+ * Module: Homework 5, Project 1
+ * Problem Statement: Calculate BMI using a GUI and show the results changing the color of a canvas
+ * 
+ * Algorithm: 
+ *  1. Create the gui with enough textboxes to take in the weight and height, two buttons (calculate BMI and exit), and textblocks
+ *     that show what the textboxes below are used for. Throw in a canvas that will change colors when the button is clicked
+ *  2. Inside the code block if the button is clicked do the following. 
+ *     Create 3 doubles named weight, height, and BMI
+ *  3. Weight and height will equal the information put into the textboxes by the user
+ *  4. BMI can be calculated with the equation (weight / height / height) * 703
+ *  5. Use if statements to determine the health of the individual. 
+ *     if BMI < 15 then change color to blue and return below average in textblock
+ *     if BMI >= 15 && BMI <=26 change color to green and return healthy average
+ *     if BMI > 26 change color to red and return higher than average
+ *  6. Display the results to the user.    
+ * 
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Project_1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void calculate_Click(object sender, RoutedEventArgs e)
+        {
+            // double variables used
+            double weight, height, BMI;
+
+            // sets the weight to the information in the textbox
+            weight = double.Parse(weight_box.Text);
+
+            // sets the height to equal the total in inches.
+            height = (double.Parse(feet_box.Text) * 12) + double.Parse(inch_box.Text);
+
+            BMI = (weight / height / height) * 703;
+            
+            // Determines what code block to use depending on the results of BMI 
+            if(BMI < 15)
+            {
+                // changes color of the canvas in the GUI
+                color.Background = Brushes.Blue;
+                // Adds text to the textblock below the GUI
+                results.Text = "Your BMI at " + BMI.ToString("F") + " is below average";
+            }
+            else if(BMI >= 15 && BMI <= 26 )
+            {
+                color.Background = Brushes.Green;
+                results.Text = "Your BMI at " + BMI.ToString("F") + " is within a healthly range";
+            }
+            else if(BMI > 26)
+            {
+                color.Background = Brushes.Red;
+                results.Text = "Your BMI at " + BMI.ToString("F") + " is above average";
+            }
+            else
+            {
+                // error message
+                results.Text = "No results found.";
+            }
+
+        }
+
+        private void exit_Click(object sender, RoutedEventArgs e)
+        {
+            // closes the program if the user clicks exit
+            this.Close();
+        }
+    }
+}
---
+++ Project_2/Program.cs
@@ -0,0 +1,89 @@
+﻿/*
+ * Project_2: This program will use a while loop to cycle through user selection and return the statistics of the users choices
+ * Name: Spencer Unitt
+ * Module: Homework 5, Project 2
+ * Problem Statement: Determine how many times the user selects heads or tails
+ * 
+ * Algorithm: 
+ *  1. Create int variables for count, heads, and tails. These are counters for the program
+ *     Two double that will be used to show the stats for heads and tails
+ *     A char variable named toss for user selection
+ *  2. Use a while loop that runs 8 times
+ *  3. In the while loop prompt the user to enter 'h' or 't' and set toss equal to it
+ *  4. if the toss is equal to h increment the heads counter by 1 
+ *     if the toss is equal to t increment the tails counter by 1
+ *  5. At the end of the while loop, increment the count by 1
+ *  6. Use the equation coin * 100 / count to determine the average.
+ *  7. Display the total amount of heads and tails selected. Show the stats for each as well
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Variables used for counters
+            int count = 0, heads = 0, tails = 0;
+
+            // Holds the stats for heads and tails
+            double heads_stats, tails_stats;
+            char toss;
+            
+            // Array that makes the program easier to read for the user.
+            string[] nums = new string[] {"First", "Second", "Third", "Fourth", "Fifth", "Sixth", "Seventh", "Eighth"};
+
+            // Instruct user what to do
+            Console.WriteLine("Enter 'h' or 't' (heads or tails) for each toss");
+            // Runs 8 times
+            while(count < 8)
+            {
+                // Prompts user for selection
+                Console.WriteLine(nums[count] + " toss: ");
+                // reads the selection and converts it to lower case 
+                toss = char.Parse(Console.ReadLine());
+                toss = char.ToLower(toss);                
+
+                // depending on the selection determine which counter to increment
+                if(toss == 'h')
+                {
+                    heads++;
+                }
+                else if(toss == 't')
+                {
+                    tails++;
+                }
+                else
+                {
+                    // print error message
+                    Console.WriteLine("Not a valid choice, try again");
+                    count--;
+                }
+
+                // increment counter by 1
+                count++;
+            }
+
+            // calculate the % of times heads and tails were selected
+            heads_stats = heads * 100 / count;
+            tails_stats = tails * 100 / count;
+
+            // displays the results to the user
+            Console.WriteLine("\n");
+            Console.WriteLine("Number of heads: " + heads);
+            Console.WriteLine("Number of tails: " + tails + "\n");
+
+            Console.WriteLine("Percent heads was selected: " + heads_stats.ToString("F"));
+            Console.WriteLine("Percent tails was selected: " + tails_stats.ToString("F"));
+
+
+            Console.ReadLine();
+        }
+    }
+}
